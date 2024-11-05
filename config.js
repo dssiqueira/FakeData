2ac736b81bf4ebde9dc2d0e268b4c72f21fdd940
Douglas Siqueira <douglas.siqueira.silva@gmail.com>
2024-11-05 18:33:13 -0300

    Limpa as estatísticas

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const saveButton = document.getElementById('save-btn');
     const backButton = document.getElementById('back-btn');
     const msgElement = document.getElementById('success-message');
+    const clearStatsButton = document.getElementById('clear-stats-btn');
 
     // Recuperar a chave do armazenamento local e definir no input
     chrome.storage.local.get(['apiKey'], function (result) {
@@ -12,10 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Salvar a chave no armazenamento local
+    // Evento para salvar a chave no armazenamento local
     saveButton.addEventListener('click', function () {
         const apiKey = inputElement.value;
-
         chrome.storage.local.set({ apiKey }, function () {
             // Mostrar mensagem de sucesso
             msgElement.classList.add('visible');
@@ -38,5 +38,16 @@ document.addEventListener('DOMContentLoaded', function () {
     // Evento para o botão de voltar
     backButton.addEventListener('click', function () {
         window.location.href = 'popup.html';
+    });
+
+    // Evento para limpar estatísticas
+    clearStatsButton.addEventListener('click', function () {
+        if (confirm("Tem certeza de que deseja limpar todas as estatísticas?")) {
+            chrome.storage.local.get(null, function (result) {
+                const keysToDelete = Object.keys(result).filter(key => key.endsWith('Count'));
+                keysToDelete.forEach(key => chrome.storage.local.remove(key));
+            });
+            alert("Estatísticas limpas com sucesso!");
+        }
     });
 });
