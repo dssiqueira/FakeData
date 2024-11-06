3f4a3631f91a9f75dfb67bc67e0ad453afa0d26f
Douglas Siqueira <douglas.siqueira.silva@gmail.com>
2024-11-05 21:31:06 -0300

    Ajustado mascara dos documentos

--- mascarar.js
+++ mascarar.js
@@ -1,27 +1,34 @@
-// Função de máscara reutilizável
-function mascararDocumento(outputId) {
-  const element = document.getElementById(outputId);
-  let value = element.textContent;
+// Adicionar eventos de clique para mascarar dados onde aplicável
+['cpf', 'cnpj', 'cep', 'cns'].forEach((id) => {
+    const mascararBtn = document.getElementById(`mascarar-${id}-btn`);
+    const element = document.getElementById(`${id}-gerado`);
+    
+    // Verifica se o botão de mascarar e o elemento de exibição existem antes de adicionar o listener
+    if (mascararBtn && element) {
+        mascararBtn.addEventListener('click', () => {
+            let value = element.textContent;
 
-  if (value.includes('.') || value.includes('-') || value.includes('/')) {
-      element.textContent = value.replace(/\D/g, ''); // Remove máscara
-  } else {
-      switch (outputId) {
-          case 'cpf-gerado':
-              element.textContent = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
-              break;
-          case 'cnpj-gerado':
-              element.textContent = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
-              break;
-          case 'cep-gerado':
-              element.textContent = value.replace(/(\d{5})(\d{3})/, '$1-$2');
-              break;
-          case 'cns-gerado':
-              element.textContent = value.replace(/(\d{3})(\d{3})(\d{3})(\d{3})(\d{3})/, '$1.$2.$3.$4.$5');
-              break;
-      }
-  }
-}
-
-// Exportar a função, se necessário
-export { mascararDocumento };
+            // Alterna entre exibir o valor mascarado e o valor desmascarado
+            if (value.includes('.') || value.includes('-') || value.includes('/')) {
+                element.textContent = value.replace(/\D/g, ''); // Remove máscara
+            } else {
+                switch (id) {
+                    case 'cpf':
+                        element.textContent = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+                        break;
+                    case 'cnpj':
+                        element.textContent = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
+                        break;
+                    case 'cep':
+                        element.textContent = value.replace(/(\d{5})(\d{3})/, '$1-$2');
+                        break;
+                    case 'cns':
+                        element.textContent = value.replace(/(\d{3})(\d{3})(\d{3})(\d{3})(\d{3})/, '$1.$2.$3.$4.$5');
+                        break;
+                }
+            }
+        });
+    } else {
+        console.warn(`Elemento ou botão de mascarar não encontrado para id: ${id}`);
+    }
+});
