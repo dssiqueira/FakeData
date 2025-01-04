d7d868dd278d5b57181503ade095f27396d4b7e5
Douglas Siqueira <douglas.siqueira.silva@gmail.com>
2025-01-04 13:03:35 -0300

    Melhora na performance do gerarCEP

--- documentos-brasil/pessoa-fisica/gerarCEP.js
+++ documentos-brasil/pessoa-fisica/gerarCEP.js
@@ -1,7 +1,7 @@
 // Função para gerar um CEP válido aleatório com base no JSON de CEPs válidos
 async function gerarCEP() {
   try {
-    const response = await fetch('https://gist.githubusercontent.com/gblmarquez/48328559f24e65974d2d/raw/fa24a5fc33a559d51af6b99a115500b7372fa580/valid_ceps.json');
+    const response = await fetch(chrome.runtime.getURL('/valid_ceps.json'));
     const ceps = await response.json();
     
     // Seleciona um CEP aleatório da lista de CEPs válidos
